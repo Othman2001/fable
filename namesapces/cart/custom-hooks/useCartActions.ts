4d0bc594a0c1d6b5359d593f1e479ed815b92858
Othman <67125450+Othman2001@users.noreply.github.com>
2022-11-06 19:28:02 +0200

    fix: type-error-in-usecartAction

--- namesapces/cart/custom-hooks/useCartActions.ts
+++ namesapces/cart/custom-hooks/useCartActions.ts
@@ -24,7 +24,7 @@ export const useCartActions = () => {
     dispatch(decrementQuantity(productId));
   };
   const changeDrawerVisibilityAction = ({
-    drawerVisibility: drawerVisibility,
+    drawerVisibility,
   }: {
     drawerVisibility: boolean;
   }) => {
